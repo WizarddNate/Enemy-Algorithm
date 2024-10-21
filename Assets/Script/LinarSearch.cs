c19e85505115d9ceec2c10fef6b4385a86dcb895
AnaMS205 <anastasia.sexson@icloud.com>
2024-10-21 14:04:50 -0500

    linear search nolonger has errer

--- Assets/Script/LinarSearch.cs
+++ Assets/Script/LinarSearch.cs
@@ -7,21 +7,22 @@ public class LinarSearch : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        #ignor unless some code for staring postion
+        //ignor unless some code for staring postion
     }
 
     // Update is called once per frame
     void Update()
     {
-        #make a function to find playerlocation
-        #for(int i = 0; < arr.Length; i++){
-            #if (arr[i] == target){
-                #return i;
-            }
-            #if it cannot find player just dont move
-        }
+        
+        // make a function to find playerlocation
+        // for(int i = 0; < arr.Length; i++){
+        //     #if (arr[i] == target){
+        //         #return i;
+        //     }
+        //     #if it cannot find player just dont move
+        // }
 
-        #once location found, move twords player
-        #check player location => move one space colser
+        // #once location found, move twords player
+        // #check player location => move one space colser
     }
 }
