b7651ecac9aa1d5bc6ccb8fa9fe1b2f29460fe02
Wizard Nate <nathanielorozco42@gmail.com>
2024-11-17 14:33:27 -0600

    Starting the Astar

---
+++ Assets/Script/A Star.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AStar
+{
+    public Pathfinding(int width, int height)
+    {
+
+    }
+}
+
--- Assets/Script/GridArray.cs
+++ Assets/Script/GridArray.cs
@@ -11,45 +11,43 @@ public class GridArray : MonoBehaviour
     public int num = 5;
 
     //Basic binary search functioin
-    class GFG
+
+
+    // Returns index of x if it is present in arr[]
+    int BinarySearch(float[] arr, int x)
     {
-
-        // Returns index of x if it is present in arr[]
-        static int binarySearch(float[] arr, int x)
+        int low = 0, high = arr.Length - 1;
+        while (low <= high)
         {
-            int low = 0, high = arr.Length - 1;
-            while (low <= high)
-            {
-                int mid = low + (high - low) / 2;
+            int mid = low + (high - low) / 2;
 
-                // Check if x is present at mid
-                if (arr[mid] == x)
-                    return mid;
+            // Check if x is present at mid
+            if (arr[mid] == x)
+                return mid;
 
-                // If x greater, ignore left half
-                if (arr[mid] < x)
-                    low = mid + 1;
+            // If x greater, ignore left half
+            if (arr[mid] < x)
+                low = mid + 1;
 
-                // If x is smaller, ignore right half
-                else
-                    high = mid - 1;
-            }
-
-            // If we reach here, then element was
-            // not present
-            return -1;
+            // If x is smaller, ignore right half
+            else
+                high = mid - 1;
         }
 
-        // Driver code
-        /*public static void Main()
+        // If we reach here, then element was
+        // not present
+        return -1;
+    }
+
+    // Driver code
+        void Start()
         {
-  
-            int result = binarySearch(gridCords, num);
+
+        int result = 0; //BinarySearch(gridCords[0,0], num);
             if (result == -1)
                 Debug.Log("Element is not present in array");
             else
                 Debug.Log("Element is present at " + "index " + result);
-        } */
-    }
+        }
 
 }
